ced1e30e9bc4a93c10d7798b47cd2369af9aa965
andrenjunior <andrenjunior@outlook.com>
2020-10-01 19:39:22 -0300

    commit

--- Tele_Aula 3/PrimeiraClasse.py
+++ Tele_Aula 3/PrimeiraClasse.py
@@ -1,6 +1,6 @@
 class primeiraclasse:
-    def imprimir_mensagem(self, nome):
+    def imprimir_mensagem(self, nome):  # Criando um método
         print(f'Olá {nome}, seja bem vindo!!')
 
-objeto1 = primeiraclasse()
-objeto1.imprimir_mensagem('André')              
+objeto1 = primeiraclasse() # Instanciando um objeto do tipo PrimeiraClasse
+objeto1.imprimir_mensagem('André')  # Invocando o método              
